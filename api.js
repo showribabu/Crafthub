5b1d6d333927f2d14116abad2ec77e1c15866338
showribabu <kantashowribabu.com>
2023-10-25 11:41:28 +0530

    changes

--- api.js
+++ api.js
@@ -25,7 +25,7 @@ api.get('/userRegister',async function(req,res){
         // {'username':'','usermail':'','userpassword':'','cuserpassword':'','phonenumber':'','profilephoto':'', 'age':'', 'area':'', 'skills':'', 'experience':'', 'workphotos':'' }
 
     var query=
-    {'username':data.query.username,'usermail':data.query.usermail,'userpassword':data.query.userpassword,'cuserpassword':data.query.cuserpassword,'phonenumber':data.query.phonenumber,'profilephoto':data.query.profilephoto, 'age':data.query.age, 'area':data.query.area, 'skills':data.query.skills, 'experience':data.query.skills, 'workphotos':data.query.workphotos};
+    {'username':data.query.username,'usermail':data.query.usermail,'userpassword':data.query.userpassword,'cuserpassword':data.query.cuserpassword,'phonenumber':data.query.phonenumber,'profilephoto':data.query.profilephoto, 'age':data.query.age, 'area':data.query.area, 'skills':data.query.skills, 'experience':data.query.experience, 'workphotos':data.query.workphotos};
     
     
     const result=await userRegister(query);
@@ -111,7 +111,7 @@ api.get('/updateDetails',async function(req,res){
     const data=url.parse(req.url,true);
     // {'username':'', 'usermail':'', 'phonenumber':'', 'profilePhoto':'', 'age':'', 'area':'', 'skills':'','experience':''}
 
-    var query={'username':data.query.username, 'usermail':data.query.usermail, 'phonenumber':data.query.phonenumber, 'profilePhoto':data.query.profilePhoto, 'age':data.query.age, 'area':data.query.area, 'skills':data.query.skills,'experience':data.query.experience};
+    var query={'username':data.query.username, 'usermail':data.query.usermail, 'phonenumber':data.query.phonenumber, 'profilephoto':data.query.profilephoto, 'age':data.query.age, 'area':data.query.area, 'skills':data.query.skills,'experience':data.query.experience};
     const result=await updateDetails(query);
     res.send(result);
 })
